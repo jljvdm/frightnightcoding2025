210e82082e8a9eb35b1431bc36dfcef15c5e2ac2
Jeroen van der Meer sr. <jeroen.van.der.meer@alliander.com>
2025-10-23 10:46:04 +0200

    Solved oktober 23

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import oktober22.Oktober22
+import oktober23.Oktober23
 
 fun main() {
     println("Hello, Fright Night Coding 2025!")
     println(Oktober22().solve())
+    println(Oktober23().solve())
 }
