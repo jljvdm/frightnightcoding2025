ecd11cecc7c31c3cad42195f44dfcc6fe36811f6
Jeroen van der Meer sr. <jeroen.van.der.meer@alliander.com>
2025-10-23 10:45:22 +0200

    Solved oktober 23

---
+++ src/main/kotlin/oktober23/Oktober23.kt
@@ -0,0 +1,34 @@
+package oktober23
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class Oktober23 {
+
+    val input: String
+
+    constructor(){
+        println("Oktober 23")
+        input = readFile()
+    }
+
+
+    fun solve(): String{
+        var opgeteldeWaardes = input.split(",").map { input -> input.toInt() }.sum()
+        while (opgeteldeWaardes < 0){
+            opgeteldeWaardes += 360
+        }
+        return (opgeteldeWaardes % 360).toString()
+    }
+
+    private fun readFile(): String {
+        val inputStream = javaClass.getResourceAsStream("/oktober23.txt")
+            ?: throw IllegalArgumentException("File not found: oktober23.txt")
+        return BufferedReader(InputStreamReader(inputStream)).readLines().first()
+    }
+}
+
+fun main() {
+    val oktober23 = Oktober23()
+    println(oktober23.solve())
+}
