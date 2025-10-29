3eccc76c4c5d6d8cb4fb83fe82ce6a2828d012cc
Jeroen van der Meer sr. <jeroen.van.der.meer@alliander.com>
2025-10-29 10:33:37 +0100

    Oktober 28 werkend

---
+++ src/main/kotlin/oktober28/Oktober28.kt
@@ -0,0 +1,32 @@
+package oktober28
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class Oktober28 {
+        val input: List<String>
+
+    constructor() {
+        println("Oktober 28")
+        input = readFile()
+    }
+
+    fun solve(): String{
+        val getal = input.first().toLong()
+        val maxInt = 4294967296L
+        val aantalMaalOverflow = getal / maxInt
+        val binaryString = java.lang.Long.toBinaryString(getal % maxInt)
+        return "$aantalMaalOverflow,$binaryString"
+    }
+
+    private fun readFile(): List<String> {
+        val inputStream = javaClass.getResourceAsStream("/oktober28.txt")
+            ?: throw IllegalArgumentException("File not found: oktober28.txt")
+        return BufferedReader(InputStreamReader(inputStream)).readLines()
+    }
+}
+
+fun main() {
+    val oktober28 = Oktober28()
+    println(oktober28.solve())
+}
